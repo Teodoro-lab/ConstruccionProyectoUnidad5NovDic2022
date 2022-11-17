0998784a27ce9409ccbc42af42f85572eeb18a3a
teodawn <teodawn.td@gmail.com>
2022-11-17 15:01:10 -0600

    refactor the names of the tests

--- git_project/src/test/java/com/proyecto/jsonTest.java
+++ git_project/src/test/java/com/proyecto/jsonTest.java
@@ -1,3 +1,53 @@
+package com.proyecto;
+
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assume.assumeNoException;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class JsonTest {
+
+    @Test
+    @DisplayName("Validate the existence of the employees JSON file")
+    public void JSONNotExistence_ThrowFileNotFoundException() {
+        assertThrows(FileNotFoundException.class, () -> {
+            try {
+                JsonValidation.readJson();
+            } catch (ParseException | IllegalArgumentException e) {
+                assumeNoException(e);
+            }
+        });
+    }
+
+    @Test
+    @DisplayName("Validate function throws ParseException when JSON file contains invalid data")
+    public void NotValidJSON_ThrowParseException() {
+        assertThrows(ParseException.class, () -> {
+            try {
+                JsonValidation.readJson();
+            } catch (IllegalArgumentException | IOException e) {
+                assumeNoException(e);
+            }
+        });
+    }
+
+    @Test
+    @DisplayName("Validate function throws IllegalArgumentException when JSON file is missing a required key")
+    public void JSONSchemaValidator_ThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            try {
+                JsonValidation.readJson();
+            } catch (ParseException | IOException e) {
+                assumeNoException(e);
+            }
+        });
+    }
+
     @Test
     @DisplayName("Validate that the that the json output string is correct")
     public void validateOutputString() {
@@ -17,3 +67,4 @@
             assumeNoException(e);
         }
     }
+}

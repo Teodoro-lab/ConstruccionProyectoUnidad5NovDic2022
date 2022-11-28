d09b63272d2c4cc72151c09ef6dfe97b0f795158
vero-wiskuchy <veronicarivera.mr@gmail.com>
2022-11-27 22:49:54 -0600

    feat: functionality updating and employee from json file

--- git_project/src/main/java/com/proyecto/JsonReader.java
+++ git_project/src/main/java/com/proyecto/JsonReader.java
@@ -1,6 +1,7 @@
 package com.proyecto;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.json.simple.JSONArray;
@@ -9,6 +10,39 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class JsonReader {
+
+    private String fileName = "src/employees.json";
+
+    private void updateEmployeeFrom(Employee e, JSONArray employees) {
+        for (int i = 0; i < employees.size(); i++) {
+            JSONObject employee = (JSONObject) employees.get(i);
+            if (employee.get("id").equals(e.getId())) {
+                employee.put("firstName", e.getFirstName());
+                employee.put("lastName", e.getLastName());
+                employee.put("photo", e.getPhoto());
+            }
+        }
+    }
+
+    public void updateEmployeeInJson(Employee e) {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader(this.fileName));
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray employees = (JSONArray) jsonObject.get("employees");
+            jsonObject.put("employees", employees);
+
+            updateEmployeeFrom(e, employees);
+
+            FileWriter file = new FileWriter(this.fileName);
+            file.write(jsonObject.toJSONString());
+            file.flush();
+            file.close();
+        } catch (IOException | ParseException e1) {
+            e1.printStackTrace();
+        }
+
+    }
 
     private Employee[] getEmployeeList(JSONObject json) {
         JSONArray employees = (JSONArray) json.get("employees");
@@ -21,7 +55,7 @@ public class JsonReader {
             String firstName = (String) employee.get("firstName");
             String lastName = (String) employee.get("lastName");
             String photo = (String) employee.get("photo");
-            
+
             employeeList[i] = new Employee(id, firstName, lastName, photo);
         }
 
